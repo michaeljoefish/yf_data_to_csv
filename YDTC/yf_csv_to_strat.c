11ae56a578af5dbaa440f13cd6bbd503c29c33fb
Michael Parks <michaeljoefish@icloud.com>
2025-05-18 13:41:17 -0400

    Added prwoso function, but this isn't fully integrated for automatic usage yet.

--- YDTC/yf_csv_to_strat.c
+++ YDTC/yf_csv_to_strat.c
@@ -22,11 +22,16 @@ typedef struct CSV_DATA{
   
 }CSV;
 
-void to_struct_arr(FILE*, CSV*, int);
+int to_struct_arr(FILE*, CSV*, int);
 void calc_all_SMA(CSV*, int);
+uint32_t prwoso(CSV*, uint8_t, uint8_t, uint16_t*, uint8_t, int);
 
-int main(void){
-  char *filepath = "OUTPUT_CSV_FILES/2025-4-16_C_15m.csv";
+int main(int argc, char **argv){
+  //char *filepath = "OUTPUT_CSV_FILES/2025-4-16_C_15m.csv";
+  char *filepath = "OUTPUT_CSV_FILES/2025/05/12_AMZN_5m.csv";
+  if (argc > 1){
+    filepath = argv[1];
+  }
   FILE* RO_INFILE = fopen(filepath, "r");
   if (!RO_INFILE){
     printf("ERROR: File Not Found");
@@ -34,7 +39,7 @@ int main(void){
   }
   int length = 500;
   CSV BUF[length];
-  to_struct_arr(RO_INFILE, BUF, length);
+  int readable_len = to_struct_arr(RO_INFILE, BUF, length);
 
   fclose(RO_INFILE);
   RO_INFILE = NULL;
@@ -56,13 +61,18 @@ int main(void){
   while( (i < length) && (BUF[i].hr != 0xff) ){
     if(BUF[i].hr == 0x00){ i++; continue;}
     
-    printf("%02hhu:%02hhu:00 %hu.%04hu , %hu.%04hu , %hu.%04hu , %hu.%04hu , %lu , %hu.%04hu , %hu.%04hu\n",
+    printf("%02hhu:%02hhu:00 , %hu.%04hu , %hu.%04hu , %hu.%04hu , %hu.%04hu , %lu , %hu.%04hu , %hu.%04hu\n",
          BUF[i].hr, BUF[i].mi, BUF[i].open[DECI], BUF[i].open[FRAC], BUF[i].high[DECI], BUF[i].high[FRAC],
            BUF[i].low[DECI], BUF[i].low[FRAC], BUF[i].close[DECI], BUF[i].close[FRAC], BUF[i].vol,
            BUF[i].ma20[DECI], BUF[i].ma20[FRAC], BUF[i].ma50[DECI], BUF[i].ma50[FRAC]);
     i++;
   }
-  
+
+  uint16_t entry_stop[] = {207,100,205,7500};
+  uint32_t pot_r = prwoso(BUF, 10, 37, entry_stop, 5, readable_len);
+
+  printf("\nlast bar = %hhu.%hhu\n", BUF[readable_len - 1].hr, BUF[readable_len - 1].mi);
+  printf("PRWOSO = %hu.%hu\n", (uint16_t)(pot_r>>16), (uint16_t)(pot_r & 0xffff));
   
 
   /*
@@ -82,11 +92,12 @@ int main(void){
   return 0;
 }
 
-void to_struct_arr(FILE* csv_file, CSV* buf, int len){
+int to_struct_arr(FILE* csv_file, CSV* buf, int len){
   // Used to store fractional parts of stats as strings.
   // Will be converted to uint16_t later.
   char r_OHLC[4][21];
 
+  int lines_read = 0;
   int result = 0;
   int ind = 0;
 
@@ -108,6 +119,9 @@ void to_struct_arr(FILE* csv_file, CSV* buf, int len){
          buf[ind].hr, buf[ind].mi, buf[ind].open[DECI], r_OHLC[0], buf[ind].high[DECI], r_OHLC[1],
          buf[ind].low[DECI], r_OHLC[2], buf[ind].close[DECI], r_OHLC[3], buf[ind].vol);*/
 
+    // Stores  number of lines read(doesn't column-name line, but does include the EOF bar in buf)
+    lines_read++;
+
     // If bad read or EOF, initialize hr to special value and continue.
     if (result != 11){
       buf[ind].hr = 0x00;
@@ -119,6 +133,7 @@ void to_struct_arr(FILE* csv_file, CSV* buf, int len){
       continue;
     }
 
+    // Set all non-digit first five characters to '0'
     for(int i = 0; i<4; i++){
       for(int k = 0; k<5; k++){
         if (r_OHLC[i][k] < 0x30 || r_OHLC[i][k] > 0x39)
@@ -168,7 +183,8 @@ void to_struct_arr(FILE* csv_file, CSV* buf, int len){
       
     ind++;
   }
-   
+
+  return lines_read - 1; 
 }
 
 void calc_all_SMA(CSV *buf, int length){
@@ -236,4 +252,61 @@ void calc_all_SMA(CSV *buf, int length){
     
     i++;
   }
+}
+
+
+
+// Gets the maximum potential R earned before stop is triggered or end of the day is reached.
+// WARNING: entry and stop fractional values must be normalized to 4 digits.
+//             ex: .92 as a fractional value should be 9200, not 0092
+uint32_t prwoso(CSV *bars, uint8_t hour, uint8_t mins, uint16_t *ExS, uint8_t tf_mins, int r_len){
+  uint16_t e_DECI = ExS[DECI];
+  uint16_t e_FRAC = ExS[FRAC];
+  uint16_t s_DECI = ExS[2 + DECI];
+  uint16_t s_FRAC = ExS[2 + FRAC];
+  uint16_t max_DECI = 0;
+  uint16_t max_FRAC = 0;
+
+  uint8_t adj_mins = (mins / tf_mins) * tf_mins;
+
+  // Work backwards (since we can't index the start directly.)
+  // If low of bar stops out, then set high as the max.
+  // Otherwise, if high of bar > max, then set high as max.
+  int i = r_len - 1;
+  while ( i>=0 && ( (bars[i].hr != hour) || (bars[i].mi != adj_mins) ) ){
+    if ( (bars[i].low[DECI] < s_DECI) || (bars[i].low[DECI] == s_DECI && bars[i].low[FRAC] <= s_FRAC) ){
+      max_DECI = bars[i].high[DECI];
+      max_FRAC = bars[i].high[FRAC];
+    }
+    else if((bars[i].high[DECI] > max_DECI) || (bars[i].high[DECI] == max_DECI && bars[i].high[FRAC] > max_FRAC) ){
+      max_DECI = bars[i].high[DECI];
+      max_FRAC = bars[i].high[FRAC];
+    }
+
+    i--;
+  }
+
+  // Index i should now be at the entry bar.
+  // If we stopped out on the entry bar (rare, but not impossible), then record the high as the max.
+  // Otherwise, check to see if it's greater than the local max.
+  if ( (bars[i].low[DECI] < s_DECI) || (bars[i].low[DECI] == s_DECI && bars[i].low[FRAC] <= s_FRAC) ){
+    max_DECI = bars[i].high[DECI];
+    max_FRAC = bars[i].high[FRAC];
+  }
+  else if ( (bars[i].high[DECI] > max_DECI) || (bars[i].high[DECI] == max_DECI && bars[i].high[FRAC] > max_FRAC) ){
+    max_DECI = bars[i].high[DECI];
+    max_FRAC = bars[i].high[FRAC];
+  }
+
+  
+  printf("Highest price WOSO = %hu.%hu\n", max_DECI, max_FRAC);
+  // Difference between entry and stop (as one number, not two uint16 parts)
+  uint32_t es_diff = (((uint32_t)(e_DECI))*10000 + e_FRAC) - (((uint32_t)(s_DECI))*10000 + s_FRAC);
+  // Difference between max price and entry.
+  uint32_t me_diff = (((uint32_t)(max_DECI))*10000 + max_FRAC) - (((uint32_t)(e_DECI)*10000) + e_FRAC);
+  // Division of the latter by the former.
+  uint32_t mees_div = (uint32_t)(((uint64_t)(me_diff))*10000 / es_diff);
+
+  // Return the result as a uint32_t, where the leftmost 16 bits are DECI, and rightmost 16 bits are FRAC.
+  return ((mees_div / 10000)<<16) | (mees_div % 10000);
 }
